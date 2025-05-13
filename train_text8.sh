3da4f8908932e3e60531c892ba7d34630af6f527
Daniil Likhobaba <daniil@likhobaba.ru>
2025-05-13 23:39:07 +0000

    small fix

--- train_text8.sh
+++ train_text8.sh
@@ -1,2 +1,6 @@
 bash setup_all.sh
+# locate libcuda.so.1
+# sudo ln -s /usr/lib/x86_64-linux-gnu/libcuda.so.1 /usr/lib/x86_64-linux-gnu/libcuda.so
+# sudo ldconfig
+export TRITON_LIBCUDA_PATH=/usr/lib/x86_64-linux-gnu/libcuda.so.1
 bash bash_scripts/train_text8.sh
