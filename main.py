f7841e9a18a255772b0877c7f6b4a645da066257
Daniil Likhobaba <daniil@likhobaba.ru>
2025-06-15 16:48:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -294,13 +294,13 @@ def _gen_ppl_eval(config, tokenizer):
     torch.tensor(tokens), return_counts=True, sorted=False)
   entropy = torch.special.entr(
     counts.float() / counts.sum()).sum().item()
-   with open(config.eval.generated_samples_path, 'w') as f:
-     json.dump({
-       'generative_ppl': generative_ppl,
-       'entropy': entropy,
-       'generated_seqs': samples,
-     },
-       f, indent=4) # type: ignore
+  with open(config.eval.generated_samples_path, 'w') as f:
+    json.dump({
+     'generative_ppl': generative_ppl,
+     'entropy': entropy,
+     'generated_seqs': samples,
+    },
+     f, indent=4) # type: ignore
   print(f"Entropy: {entropy:0.3f}")
   print(f"Gen. PPL: {generative_ppl:0.3f}")
 
